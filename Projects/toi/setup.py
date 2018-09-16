9ede7db7f0d7ac858e761974f022c3c105b67417
Rahul <rahul1985.datascientist@gmail.com>
2018-09-16 18:45:49 +0530

    NLP sync folder

---
+++ Projects/toi/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='News Headlines Of India.16 years of categorized headlines focusing on India',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
